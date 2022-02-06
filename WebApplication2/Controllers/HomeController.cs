66fac94d72d213a965ddfe209e11b52434d20360
Mutter0815 <mutter8525@yandex.ru>
2022-02-06 16:44:18 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using Partyinvites.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public  ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            Repository.AddResponse(guest);
+            return View("Thanks",guest);
+        }
+        public ViewResult ListResponses()
+        {
+            return View("ListResponses",Repository.Responses.Where(r=>r.WillAttend==true));
+        }
+    }
+}
