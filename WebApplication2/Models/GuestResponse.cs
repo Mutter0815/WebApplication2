79a8c3b27974c35b14ed10eb1e88fa449d597ccc
Mutter0815 <mutter8525@yandex.ru>
2022-02-07 22:29:36 +0300

    1

--- WebApplication2/Models/GuestResponse.cs
+++ WebApplication2/Models/GuestResponse.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Partyinvites.Models { 
 public class GuestResponse
 {
+        [Required(ErrorMessage ="Please enter your name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your Email")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email adress")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
 
 
